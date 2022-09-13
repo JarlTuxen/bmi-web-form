8e6ae3e3ab72ce2bdec520705f010acb2a32352d
jarltuxen <jarl.tuxen@gmail.com>
2022-09-13 14:30:16 +0200

    Initial commit

---
+++ src/main/java/dk/kea/bmiwebform/BmiWebFormApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.bmiwebform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiWebFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmiWebFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/bmiwebform/BmiWebFormApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.bmiwebform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiWebFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
